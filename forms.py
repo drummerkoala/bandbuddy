00a711fde39e09c7224f1a8dae8ee8454bb453a2
drummerkoala <43947987+drummerkoala@users.noreply.github.com>
2021-01-16 17:29:41 +0300

    Added forms for new user, new band and requests

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, NumberRange, Optional
-from wtforms_components import IntegerField
+from wtforms.validators import DataRequired, NumberRange, Optional, Length, Email, InputRequired
+from wtforms_components import IntegerField, SelectField
 
 from datetime import datetime
 
@@ -16,6 +16,31 @@ class MovieEditForm(FlaskForm):
             NumberRange(min=1887, max=datetime.now().year),
         ],
     )
+
+class NewUserForm(FlaskForm):
+    user_name = StringField("Name", validators=[DataRequired(), Length(max = 50)])
+    email = StringField("E-mail", validators=[Email(message = "Please enter a valid e-mail."), Length(max = 50)])
+    age = IntegerField("Age", validators=[NumberRange(min=12, max = 99)])
+    gender = SelectField("Gender", choices=['Male', 'Female'])
+    instrument = SelectField(u'Instrument', choices=['Drums', 'Vocals', 'Electroguitar', 'Bass Guitar', 'Piano'])
+    city = SelectField(u'City', choices=['Istanbul', 'Denizli', 'Izmir', 'Ankara', 'Antalya'])
+    level = SelectField(u'Level', choices=['Basic', 'Novice', 'Intermediate', 'Advanced', 'Expert'])
+    goal = SelectField(u'Goal', validators=[Optional()], choices=['Fun', 'Socializing', 'Concerts - Serious', 'Concerts - Relaxed'])
+
+class NewBandForm(FlaskForm):
+    band_name = StringField("Band Name", validators=[DataRequired(), Length(max = 50)])
+    city = SelectField(u'City', choices=['Istanbul', 'Denizli', 'Izmir', 'Ankara', 'Antalya'])
+    genre = SelectField(u'Genre', choices=['Blues', 'Country', 'Dance', 'Electronic', 'Hip Hop', 'Jazz', 'Latin', 'Pop', 'Rap', 'Rock', 'Other'])
+    level = SelectField(u'Level', choices=['Basic', 'Novice', 'Intermediate', 'Advanced', 'Expert'])
+
+class BandRequestForm(FlaskForm):
+    goal = SelectField(u'Goal', validators=[Optional()], choices=['Fun', 'Socializing', 'Concerts - Serious', 'Concerts - Relaxed'])
+    genre = SelectField(u'Genre', choices=['Blues', 'Country', 'Dance', 'Electronic', 'Hip Hop', 'Jazz', 'Latin', 'Pop', 'Rap', 'Rock', 'Other'])
+    
+class MemberRequestForm(FlaskForm):
+    instrument = SelectField(u'Instrument', choices=['Drums', 'Vocals', 'Electroguitar', 'Bass Guitar', 'Piano'])
+    pref_gender = SelectField("Gender", validators=[Optional()], choices=['Male', 'Female'])
+    goal = SelectField(u'Goal', validators=[Optional()], choices=['Fun', 'Socializing', 'Concerts - Serious', 'Concerts - Relaxed'])
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
